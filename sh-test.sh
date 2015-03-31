da6d839856c1e5d2c61e3f0beb0f5f83d6f763aa
Michael Reed <mreed@gilt.com>
2015-03-31 06:09:41 -0400

    throw away output of pushd and popd

--- sh-test.sh
+++ sh-test.sh
@@ -7,9 +7,9 @@ export PATH="$ROOT/bin:$PATH"
 cd sh-test
 for example in *; do
   if [ -d $example ]; then
-    pushd $example
+    pushd >/dev/null $example
       echo "Running example in $example"
       bash -ex run.sh 2>&1 | diff expected.txt -
-    popd
+    popd >/dev/null
   fi
 done
